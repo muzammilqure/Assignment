2a3928bb700e7994aeb6ab7868586b7e1527c762
muzammilqure <muzammilq78@gmail.com>
2024-01-05 17:55:48 +0530

    changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ title }) => {
   return (
-    <button className="bg-blue-300 text-white rounded-md p-2 h-20 w-30text-lg font-semibold font-palanquin hover:bg-blue-500 hover:scale-105 transition-all">
+    <button className="bg-blue-300 text-white rounded-md p-2 h-20 w-30 text-lg font-semibold font-palanquin hover:bg-blue-500 hover:scale-105 transition-all">
       {title}
     </button>
   );
